ca3fc2a7d9a8192a2d0fce30403778cb479a2472
Sumanth Maddirala <sumanthm@princeton.edu>
2021-03-31 06:57:48 -0400

    added DHall API request statements

--- server.py
+++ server.py
@@ -2,7 +2,21 @@ from flask import Flask, request, make_response, redirect, url_for
 from flask import render_template
 from database import Database
 from datetime import datetime
+import requests
 
+# please note: this is not a permanent access token... it needs to be refreshed a ton (1000 hrs)
+configs = {"BASE_URL": "https://api.princeton.edu/mobile-app/1.0.0/",
+    "ACCESS_TOKEN": "NGE3YjBkYjgtZDcwMy0zOTRhLWIzOWUtNTNhZGM5MTFmMzQ4OnRpZ2VydG9vdGhAY2FyYm9uLnN1cGVy"}
+
+req = requests.get(
+    configs["BASE_URL"] + "dining/menu",
+    params={ "locationId" : "whitman", "menuID": "2019-03-15-LUNCH"},
+    headers={
+        "Authorization": "Bearer " + configs["ACCESS_TOKEN"]
+    },
+)
+text = req.text
+print(text)
 
 app = Flask(__name__, template_folder='.')
 app.static_folder = 'static'
