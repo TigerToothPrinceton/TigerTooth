bc806c7468233ec82036649b17d6d412810b69b8
davidsliu22 <66916805+davidsliu22@users.noreply.github.com>
2021-04-14 15:05:35 -0400

    Update server.py

--- server.py
+++ server.py
@@ -108,7 +108,8 @@ def food():
             meal = "Lunch"
         elif (time_hour >= 14 and time_hour < 20):
             meal = "Dinner"
-
+        database.clear_db(meal)
+            
         # Get locationID
         locationID = 1
         if dhall == "wilcox":
