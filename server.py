141aa6c6341a1d8e803b99dd14ce0741c624530b
davidsliu22 <66916805+davidsliu22@users.noreply.github.com>
2021-04-21 14:48:04 -0400

    Update server.py

--- server.py
+++ server.py
@@ -157,7 +157,7 @@ def food():
         rows = database.get_reactions(dhall)
         database.disconnect()
         html = render_template('food.html', foods=foods, rows=rows,
-                               college=dhall, meal_time=meal, username=username)
+                               college=dhall, meal_time=meal)
         response = make_response(html)
         return response
     except Exception as e:
