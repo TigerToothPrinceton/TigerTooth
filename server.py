5cfae9ff5883a9de41542fbd234c84d50bc35c54
davidsliu22 <66916805+davidsliu22@users.noreply.github.com>
2021-04-26 04:35:07 -0400

    Update server.py

--- server.py
+++ server.py
@@ -153,7 +153,7 @@ def food():
             api_id = food['id']
             result = database.get_food(api_id, dhall)
             review = database.get_reviews(result[4])
-            print(result)
+            result.append(review)
             foods.append(result)
           
         rows = database.get_reactions(dhall)
