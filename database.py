ab4b52380ea06ed0368104a4bd840a256fc0412d
Sam Liang <darkdragn20000@gmail.com>
2021-04-26 17:20:45 -0400

    used prepared statements for all SQL statements

--- database.py
+++ database.py
@@ -38,9 +38,8 @@ class Database():
     def get_reactions(self, dhall):
         try:
             cursor = self._connection.cursor()
-            get_query = "SELECT * FROM reactions WHERE reactions.dhall='{}' ORDER BY reactions.reactions_id DESC".format(
-                dhall)
-            cursor.execute(get_query)
+            get_query = "SELECT * FROM reactions WHERE reactions.dhall=%s ORDER BY reactions.reactions_id DESC"
+            cursor.execute(get_query, [dhall])
             return cursor.fetchall()
         except Exception as e:
             print(f'{e}', file=stderr)
@@ -52,9 +51,8 @@ class Database():
             est = pytz.timezone('US/Eastern')
             for new_food in new_foods:
                 # is the new food already in the food table
-                boolean_query = "SELECT EXISTS(SELECT 1 FROM food WHERE api_id='{}' and dhall='{}')".format(
-                    new_food['id'], dhall)
-                cursor.execute(boolean_query)
+                boolean_query = "SELECT EXISTS(SELECT 1 FROM food WHERE api_id=%s and dhall=%s)"
+                cursor.execute(boolean_query, [new_food['id'], dhall])
                 if cursor.fetchone()[0] == False:
                     insert_query = "INSERT INTO food (name, num_ratings, num_stars, dhall, last_served, api_id) VALUES (%s, %s, %s, %s, %s, %s)"
                     insert_arr = [new_food['name'], 0, 0, dhall, datetime.today().astimezone(est).strftime(
@@ -69,9 +67,8 @@ class Database():
     def get_food(self, api_id, dhall):
         try:
             cursor = self._connection.cursor()
-            get_query = "SELECT food.url, food.name, food.num_ratings, food.num_stars, food.food_id, food.api_id FROM food WHERE food.api_id='{}' and food.dhall='{}'".format(
-                api_id, dhall)
-            cursor.execute(get_query)
+            get_query = "SELECT food.url, food.name, food.num_ratings, food.num_stars, food.food_id, food.api_id FROM food WHERE food.api_id=%s and food.dhall=%s"
+            cursor.execute(get_query, [api_id, dhall])
             return cursor.fetchone()
         except Exception as e:
             print(f'{e}', file=stderr)
@@ -81,9 +78,8 @@ class Database():
     def get_foods(self, dhall):
         try:
             cursor = self._connection.cursor()
-            get_query = "SELECT food.url, food.name, food.num_ratings, food.num_stars, food.food_id, food.api_id FROM food WHERE food.dhall='{}'".format(
-                dhall)
-            cursor.execute(get_query)
+            get_query = "SELECT food.url, food.name, food.num_ratings, food.num_stars, food.food_id, food.api_id FROM food WHERE food.dhall=%s"
+            cursor.execute(get_query, [dhall])
             return cursor.fetchall()
         except Exception as e:
             print(f'{e}', file=stderr)
@@ -92,9 +88,8 @@ class Database():
     def get_food_info(self, food_id):
         try:
             cursor = self._connection.cursor()
-            get_query = "SELECT food.url, food.name, food.num_ratings, food.num_stars FROM food WHERE food.food_id='{}'".format(
-                food_id)
-            cursor.execute(get_query)
+            get_query = "SELECT food.url, food.name, food.num_ratings, food.num_stars FROM food WHERE food.food_id=%s"
+            cursor.execute(get_query, [food_id])
             return cursor.fetchall()
         except Exception as e:
             print(f'{e}', file=stderr)
@@ -105,9 +100,8 @@ class Database():
         try:
             cursor = self._connection.cursor()
             ## name, ingredients, numRatings, numStars, description, url, dhall, lastServed
-            get_query = "SELECT reviews.review FROM reviews WHERE reviews.food_id='{}' ORDER BY reviews.time DESC, reviews.review_id DESC".format(
-                food_id)
-            cursor.execute(get_query)
+            get_query = "SELECT reviews.review FROM reviews WHERE reviews.food_id=%s ORDER BY reviews.time DESC, reviews.review_id DESC"
+            cursor.execute(get_query, [food_id])
             return cursor.fetchall()
         except Exception as e:
             print(f'{e}', file=stderr)
@@ -117,39 +111,31 @@ class Database():
     def add_review(self, review_data, rating, food_id):
         try:
             cursor = self._connection.cursor()
-            boolean_query = "SELECT EXISTS(SELECT 1 FROM reviews WHERE net_id='{}' and food_id='{}')".format(
-                review_data[0], review_data[1])
-            cursor.execute(boolean_query)
+            boolean_query = "SELECT EXISTS(SELECT 1 FROM reviews WHERE net_id=%s and food_id=%s)"
+            cursor.execute(boolean_query, [review_data[0], review_data[1]])
             # user never reviews this food
             if cursor.fetchone()[0] == False:
                 insert_query = "INSERT INTO reviews (net_id, food_id, review, rating, time) VALUES (%s, %s, %s, %s, %s)"
-                update_query = "UPDATE food SET num_ratings = num_ratings + 1, num_stars = num_stars + '{}' WHERE food.food_id = '{}'".format(
-                    rating, food_id)
+                update_query = "UPDATE food SET num_ratings = num_ratings + 1, num_stars = num_stars + %s WHERE food.food_id = %s"
                 cursor.execute(insert_query, review_data)
-                cursor.execute(update_query)
+                cursor.execute(update_query, [rating, food_id])
                 self._connection.commit()
 
-                get_query = "SELECT review_id FROM reviews WHERE net_id='{}' and food_id='{}'".format(
-                    review_data[0], review_data[1])
-                cursor.execute(get_query)
+                get_query = "SELECT review_id FROM reviews WHERE net_id=%s and food_id=%s"
+                cursor.execute(get_query, [review_data[0], review_data[1]])
                 review_id = cursor.fetchone()[0]
-                update_query = "UPDATE users SET rev_rate = array_append(rev_rate, '{}') WHERE net_id = '{}'".format(
-                    review_id, review_data[0])
-                cursor.execute(update_query)
+                update_query = "UPDATE users SET rev_rate = array_append(rev_rate, %s) WHERE net_id = %s"
+                cursor.execute(update_query, [review_id, review_data[0]])
                 self._connection.commit()
             else:
-                get_query = "SELECT rating FROM reviews WHERE net_id='{}' and food_id='{}'".format(
-                    review_data[0], review_data[1])
-                cursor.execute(get_query)
+                get_query = "SELECT rating FROM reviews WHERE net_id=%s and food_id=%s"
+                cursor.execute(get_query, [review_data[0], review_data[1]])
                 old_rating = cursor.fetchone()[0]
-                # update_query = "UPDATE reviews SET review = '{}', rating = '{}', time = '{}' WHERE reviews.net_id = '{}' AND reviews.food_id = '{}'".format(
-                #     review_data[2], rating, review_data[4], review_data[0], review_data[1])
-                update_query = "UPDATE reviews SET review = (%s), rating = (%s), time = (%s) WHERE reviews.net_id = (%s) AND reviews.food_id = (%s)"
+                update_query = "UPDATE reviews SET review = %s, rating = %s, time = %s WHERE reviews.net_id = %s AND reviews.food_id = %s"
                 cursor.execute(update_query, [
                                review_data[2], rating, review_data[4], review_data[0], review_data[1]])
-                update_query = "UPDATE food SET num_stars = num_stars - '{}' + '{}' WHERE food.food_id = '{}'".format(
-                    old_rating, rating, food_id)
-                cursor.execute(update_query)
+                update_query = "UPDATE food SET num_stars = num_stars - %s + %s WHERE food.food_id = %s"
+                cursor.execute(update_query, [old_rating, rating, food_id])
                 self._connection.commit()
         except Exception as e:
             print(f'{e}', file=stderr)
@@ -159,9 +145,8 @@ class Database():
     def add_food_image(self, api_id, food_url):
         try:
             cursor = self._connection.cursor()
-            update_query = "UPDATE food SET url = '{}' WHERE food.api_id = '{}'".format(
-                food_url, api_id)
-            cursor.execute(update_query)
+            update_query = "UPDATE food SET url = %s WHERE food.api_id = %s"
+            cursor.execute(update_query, [food_url, api_id])
             self._connection.commit()
         except Exception as e:
             print(f'{e}', file=stderr)
@@ -182,6 +167,7 @@ class Database():
             if meal == "Dinner":
                 time1 = 14
                 time2 = 20
+            # do not use .format for the SQL statements; use %s (see code above for example)
             delete_query = "DELETE from reactions WHERE ".format(
                 food_url, api_id)
             cursor.execute(update_query)
@@ -195,9 +181,8 @@ class Database():
         try:
             cursor = self._connection.cursor()
             # is this user already in the users table
-            boolean_query = "SELECT EXISTS(SELECT 1 FROM users WHERE net_id='{}')".format(
-                netid)
-            cursor.execute(boolean_query)
+            boolean_query = "SELECT EXISTS(SELECT 1 FROM users WHERE net_id=%s)"
+            cursor.execute(boolean_query, [netid])
             if cursor.fetchone()[0] == False:
                 insert_query = "INSERT INTO users (net_id) VALUES (%s)"
                 cursor.execute(insert_query, [netid])
@@ -210,24 +195,16 @@ class Database():
     def get_history(self, net_id):
         try:
             cursor = self._connection.cursor()
-            ## name, ingredients, numRatings, numStars, description, url, dhall, lastServed
-            # get_query = "SELECT rev_rate FROM users WHERE net_id = '{}'".format(
-            #     net_id)
-            # cursor.execute(get_query)
-            # reviews_arr = cursor.fetchone()[0]
-            # for review_id in reviews_arr:
-            get_query = "SELECT food_id, review, rating FROM reviews WHERE net_id = '{}'".format(
-                net_id)
-            cursor.execute(get_query)
+            get_query = "SELECT food_id, review, rating FROM reviews WHERE net_id = %s"
+            cursor.execute(get_query, [net_id])
             reviews_arr = cursor.fetchall()
             result_arr = []
             for reviews in reviews_arr:
                 food_id = reviews[0]
                 review = reviews[1]
                 rating = reviews[2]
-                get_query = "SELECT name, url, dhall, api_id FROM food WHERE food_id = '{}'".format(
-                    food_id)
-                cursor.execute(get_query)
+                get_query = "SELECT name, url, dhall, api_id FROM food WHERE food_id = %s"
+                cursor.execute(get_query, [food_id])
                 result = list(cursor.fetchone())
                 result.append(rating)
                 result.append(review)
